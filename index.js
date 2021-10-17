a64a017c54c4d72fec4b6070b707e0e36961b84e
王静宇 <wangjingyu@wangjingyudeMacBook-Pro.local>
2021-10-17 16:59:01 +0800

    add feature

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+var online = 0;
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+  io.emit("online", ++online);
   socket.on("disconnect", () => {
     console.log("user disconnected");
+    io.emit("online", --online);
+    socket.broadcast.emit("out", "a user disconnected");
   });
 });
 
@@ -24,7 +29,7 @@ io.on("connection", (socket) => {
 });
 
 io.on("connection", (socket) => {
-  socket.broadcast.emit("hi");
+  socket.broadcast.emit("in", "a user connected");
 });
 
 server.listen(3000, () => {
